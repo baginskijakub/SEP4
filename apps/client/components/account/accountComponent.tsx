d92d5d1bab7904f611ed852c84abff1631dc786d
Rosabm <rosabrialesm@gmail.com>
2023-04-07 21:04:26 +0200

    account component impl
    Missing:
    -display errors
    -make the opacity of the component not change
    -text connnection to cloud
    Ill keep working tomorrow

--- apps/client/components/account/accountComponent.tsx
+++ apps/client/components/account/accountComponent.tsx
@@ -2,44 +2,49 @@ import React from "react";
 import styles from "./account.module.css";
 import {MdOutlineMail, MdLock, MdVisibility, MdVisibilityOff} from 'react-icons/md'
 import { useState } from "react";
+import { login } from "../../services/LoginService";
+import { register } from "../../services/RegisterService";
+
 
 interface Props{
   userStatus: string;
+  onClick:(buttonType: string) => void
 }
 
 
-export const ManageAccount:React.FC<Props> = ({userStatus}) => {
+export const ManageAccount:React.FC<Props> = ({userStatus, onClick}) => {
     const [pswrdVisibility, setPswrdVisibility] = useState("password");
 
  
  function loginBtnClicked(){
-  console.log("login clicked");
- }
+  const email= document.querySelector<HTMLInputElement>('#email').value
+  const password=document.querySelector<HTMLInputElement>('#password').value
+  login(email, password)
+  .then (response => {
+    const token  =  response.token;
+    localStorage.setItem("token", token);
+}).catch(err => console.log(err));
+};
 
- function wantToLoginBtnClicked(){
-    console.log("want to log in clicked");
-   }
 
  function signUpBtnClicked(){
-    console.log("sign up clicked");
-   }
-
-   function wantToSignUpBtnClicked(){
-    console.log("want tp sign up clicked");
-   }
-
+  const email= document.querySelector<HTMLInputElement>('#email').value
+  const password=document.querySelector<HTMLInputElement>('#password').value
+  register(email, password)  
+}
 
 
  function togglePassword()
  {
- const visibility = pswrdVisibility === 'password' ? 'text' : 'password';
+  const visibility = pswrdVisibility === 'password' ? 'text' : 'password';
   setPswrdVisibility( visibility);
  }
- 
-  if(userStatus=="wantToLogin")
+
+  if (userStatus=="wantToLogin")
   return (
     <>
-    <div className={styles.accountWrapper}>
+    <div className={styles.accountWrapper} id="accountWrapper">
+      <div className="closeBtn" onClick={(e) => onClick("closeBtn")}>X</div>
       <h1>Login</h1>
       <div className={styles.accountForm}>
       <div className={styles.inputWrapper}>
@@ -47,6 +52,7 @@ export const ManageAccount:React.FC<Props> = ({userStatus}) => {
           <input placeholder="E-Mail"
               name="email"
               type="text"
+              id="email"
             />          
           </div>
           <div className={styles.inputWrapper}>
@@ -56,6 +62,7 @@ export const ManageAccount:React.FC<Props> = ({userStatus}) => {
             placeholder="Password"
               name="password"
               type={pswrdVisibility}
+              id="password"
             />
            {(pswrdVisibility=="password")?<MdVisibilityOff size={20} className={styles.icon} onClick={togglePassword}/>:<MdVisibility size={20} className={styles.icon} onClick={togglePassword}/>} 
           </div>
@@ -64,23 +71,26 @@ export const ManageAccount:React.FC<Props> = ({userStatus}) => {
           <div  className={styles.accountBtnContainer}>
             <button className={styles.accountBtn} onClick={loginBtnClicked}>Login</button>
         
-            <p className={styles.accountText}>Don&apos;t have an account? <label className={styles.signUpBtn} onClick={wantToSignUpBtnClicked}>Sign up here!</label> </p>
+            <p className={styles.accountText}>Don&apos;t have an account? <label className={styles.signUpBtn} onClick={(e) => onClick("wantToSignUpBtn")}>Sign up here!</label> </p>
          </div> 
    </div>
     </>
   );
+
   else if (userStatus=="wantToSignUp")
   {
     return (
         <>
-        <div className={styles.accountWrapper}>
-          <h1>Sign Up</h1>
+      
+        <div className={styles.accountWrapper} id="accountWrapper">
+           <div className="closeBtn" onClick={(e) => onClick("closeBtn")}>X</div> <h1>Sign Up</h1>
           <div className={styles.accountForm}>
           <div className={styles.inputWrapper}>
             <MdOutlineMail size={20} className={styles.icon}/>
               <input placeholder="E-Mail"
                   name="email"
                   type="text"
+                  id="email"
                 />          
               </div>
               <div className={styles.inputWrapper}>
@@ -90,6 +100,7 @@ export const ManageAccount:React.FC<Props> = ({userStatus}) => {
                 placeholder="Password"
                   name="password"
                   type={pswrdVisibility}
+                  id="password"
                 />
                {(pswrdVisibility=="password")?<MdVisibilityOff size={20} className={styles.icon} onClick={togglePassword}/>:<MdVisibility size={20} className={styles.icon} onClick={togglePassword}/>} 
               </div>
@@ -97,11 +108,11 @@ export const ManageAccount:React.FC<Props> = ({userStatus}) => {
               <label id="errorMsg"></label>
               <div  className={styles.accountBtnContainer}>
                 <button className={styles.accountBtn} onClick={signUpBtnClicked}>Sign Up</button>
-            
-                <p className={styles.accountText}>Already have an account? <label className={styles.signUpBtn} onClick={wantToLoginBtnClicked}>Login here!</label> </p>
+                <p className={styles.accountText}>Already have an account? <label className={styles.signUpBtn} onClick={(e) => onClick("wantToLoginBtn")}>Login here!</label> </p>
              </div> 
        </div>
         </>)
   }
+  //else is either login or not login
 else return;
 };
--- apps/client/components/navigation/navbar/Navbar.tsx
+++ apps/client/components/navigation/navbar/Navbar.tsx
@@ -6,21 +6,51 @@ import { RiPlantFill } from "react-icons/ri";
 import { UserButton } from "../userButton/UserButton";
 import { useState } from "react";
 import { ManageAccount } from "../../account/accountComponent";
+
 export const Navbar:React.FC = () => {
 //this state is managing the visibility of the user button and the login and createAccount component
 //it can be notLogin, wantToLogIn, login , wantToRegister
-
   const [userStatus, setUserStatus] = useState("notLogin");
 
+  function logout(){
+    localStorage.clear();}
 
   function userBtnClicked()
   {
-   if (userStatus=="notLogin") {setUserStatus("wantToLogin")}
-   else{setUserStatus("notLogin")}
+    const token = localStorage.getItem("auth");
+    if (token) {
+      setUserStatus("notLogin");
+    } else {
+      document.body.style.opacity = "0.3";
+      setUserStatus("wantToLogin")};
   }
+     
+      const handleAcountClicks = (btnType: string) => {
+        {
+          if (btnType=="wantToSignUpBtn")
+          {
+          setUserStatus("wantToSignUp")
+          console.log("want to su")
+          document.body.style.opacity = "0.3";
+          document.getElementById('accountWrapper').style.opacity= "1";
+        }
+          else if (btnType=="wantToLoginBtn")
+        {
+        setUserStatus("wantToLogin")
+        console.log("want to login")
+        document.body.style.opacity = "0.3";
+        document.getElementById("accountWrapper").style.opacity= "1";
+    
+        }
+        else if (btnType=="closeBtn")
+        {setUserStatus("notLogin")
+        document.body.style.opacity = "1";
+        console.log("want to close")}
+      };
+     
+    };
+          
   
-
-
   return (
     <div className={styles.navbarWrapper}>
       <h1>Plantify</h1>
@@ -42,10 +72,10 @@ export const Navbar:React.FC = () => {
           <p>Tasks</p>
         </NavItem>
       </div>
-      <UserButton userStatus={userStatus} onClick={userBtnClicked}/>    
-      <ManageAccount userStatus={userStatus}/>
+      <UserButton userStatus={userStatus} onClick={userBtnClicked} onLogoutClick={logout}/>    
+      <ManageAccount userStatus={userStatus} onClick={handleAcountClicks}/>
     </div>
   );
-};
+;}
 
 
