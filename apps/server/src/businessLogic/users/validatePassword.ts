2dcd79a53ced1391cda8a23d7b482254ff4b11b7
Hedrekao <82450784+Hedrekao@users.noreply.github.com>
2023-04-10 21:17:06 +0200

    fix to regex

--- apps/server/src/businessLogic/users/validatePassword.ts
+++ apps/server/src/businessLogic/users/validatePassword.ts
@@ -1,4 +1,5 @@
 export function validatePassword(password: string): boolean {
-  const passwordRegex = /^(?=.*[a-z])(?=.*[`A`-Z])(?=.*\d)[a-zA-Z\d]{8,}$/
+  // 8 characters, 1 uppercase, 1 lowercase, 1 number
+  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/
   return passwordRegex.test(password)
 }
