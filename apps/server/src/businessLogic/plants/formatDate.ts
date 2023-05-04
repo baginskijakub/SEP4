1924027deec6f6ef9a711318274b7ab7fae01e7f
Julia Papiernik <jullipap@gmail.com>
2023-05-04 19:39:38 +0200

    correct format of date to display in graph

--- apps/server/src/businessLogic/plants/formatDate.ts
+++ apps/server/src/businessLogic/plants/formatDate.ts
@@ -1,15 +1,11 @@
 export function formatDate(dateEpoch: number): string {
-  // i want to return a string telling me how many hours have passed since the dateEpoch, or if it's more than 24 hours, how many days have passed
-  const date = new Date(dateEpoch)
   const now = new Date()
-  const diff = now.getTime() - date.getTime()
-  const hours = Math.floor(diff / 1000 / 60 / 60)
-  const days = Math.floor(hours / 24)
-  if (days > 0) {
-    return `${days} days ago`
+  const dt = new Date(dateEpoch * 1000)
+  const diff = now.getTime() - dt.getTime()
+
+  if (diff < 86400000) {
+    return dt.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
+  } else {
+    return dt.toLocaleDateString()
   }
-  if (hours > 0) {
-    return `${hours} hours ago`
-  }
-  return 'less than hour ago'
 }
