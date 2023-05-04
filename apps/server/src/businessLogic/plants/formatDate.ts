a4b5057cf7bca45866c7bbc02111f3b1c1a8ccc6
Julia Papiernik <jullipap@gmail.com>
2023-05-04 19:56:02 +0200

    validation of epoch added

--- apps/server/src/businessLogic/plants/formatDate.ts
+++ apps/server/src/businessLogic/plants/formatDate.ts
@@ -1,6 +1,12 @@
 export function formatDate(dateEpoch: number): string {
   const now = new Date()
   const dt = new Date(dateEpoch * 1000)
+  if (isNaN(dt.getTime())) {
+    return 'Invalid date'
+  }
+  if (dt.getTime() > now.getTime()) {
+    return 'Invalid date'
+  }
   const diff = now.getTime() - dt.getTime()
 
   if (diff < 86400000) {
