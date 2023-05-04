936a320b70634092099057f4ce7944593f217748
Franciszek Nurkiewicz <318212@via.dk>
2023-05-04 11:06:52 +0200

    socket.io setup init

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -4,10 +4,15 @@ import cookieParser from 'cookie-parser'
 import userRouter from './controllers/usersController'
 import plantsRouter from './controllers/plantsController'
 import cors from 'cors'
+import http from 'http'
+import {Server} from 'socket.io'
+
 
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` })
 
 export const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
 
 app.use(cors({ origin: 'http://localhost:4200', credentials: true }))
 app.use(express.json())
@@ -16,3 +21,19 @@ app.use(cookieParser())
 app.use('/api/v1/users', userRouter)
 
 app.use('/api/v1/plants', plantsRouter)
+
+io.on('connect', (socket) => {
+    console.log('Client connected')
+    socket.on('data_Receive', (data) =>{
+        console.log('Data received:', data)
+        socket.emit('response', {message: 'test response'})
+    })
+    socket.on('disconnect', () => {
+        console.log('Client disconnected')
+    })
+})
+
+const port = process.env.PORT || 3000
+server.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+})
