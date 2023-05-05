64b703154f056f8bc193775de7ef97001ac1b337
Franciszek Nurkiewicz <318212@via.dk>
2023-05-05 20:29:29 +0200

    attempt to cache init

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -3,7 +3,9 @@ import { app } from './server'
 import { handleMessageEvent } from './businessLogic/lorawan/handleMessageEvent'
 import http from 'http'
 import { Server } from 'socket.io'
+import NodeCache from 'node-cache'
 
+const cache = new NodeCache();
 export const lorawanSocket = new WebSocket(process.env.LORAWAN_SOCKET_URL)
 
 lorawanSocket.on('open', () => {
@@ -14,6 +16,7 @@ lorawanSocket.on('message', (data) => {
   const message = JSON.parse(data.toString())
   console.log('Lorawan socket message received', message)
   handleMessageEvent(message)
+  io.emit('lorawan_message', message)
 })
 
 lorawanSocket.on('close', async () => {
@@ -29,6 +32,11 @@ const io = new Server(server)
 
 io.on('connect', (socket) =>{
   console.log('Client connected')
+  const cachedData = cache.get(socket.id)
+  if(cachedData){
+    console.log(`Sending data to id ${socket.id}`, cachedData)
+    socket.emit('cached_data',cachedData)
+  }
   socket.on('disconnect', () =>{
     console.log('Client disconnected')
   })
