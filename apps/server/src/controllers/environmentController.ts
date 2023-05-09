725314365f71e358ef18ad3b5806d3a2dcf00a97
Julia Papiernik <jullipap@gmail.com>
2023-05-09 11:59:46 +0200

    patch endpoint to send data to lorawan

--- apps/server/src/controllers/environmentController.ts
+++ apps/server/src/controllers/environmentController.ts
@@ -5,16 +5,30 @@ import { isValidType } from '../businessLogic/environment/isValidGraphType'
 import { formatDate } from '../businessLogic/plants/formatDate'
 import authorizeUser, { UserRequest } from '../middleware/authorizeUser'
 import { isValidDesiredEnv } from '../businessLogic/environment/isValidDesiredEnvironment'
+import { sendDownlinkMessage } from '../businessLogic/lorawan/sendMessage'
 
 const environmentRouter = express.Router({ mergeParams: true })
 
 environmentRouter.use(authorizeUser)
 
-environmentRouter.post('/', async (req: UserRequest, res) => {
-    const { plantId } = req.params
-    const desiredEnvironment = req.body as unknown
-    if(!desiredEnvironment || !isValidDesiredEnv) return res.status(400).json({message: 'Invalid desired environment', status: 'error'})
-    
+environmentRouter.patch('/', async (req: UserRequest, res) => {
+  const { plantId } = req.params
+  const desiredEnvironment = req.body as unknown
+  if (!plantId || isNaN(Number(plantId))) return res.status(400).json({ message: 'Missing plant id', status: 'error' })
+  if (!desiredEnvironment || !isValidDesiredEnv(desiredEnvironment))
+    return res.status(400).json({ message: 'Invalid desired environment', status: 'error' })
+
+  const humidityHex = (desiredEnvironment.humidity * 10).toString(16).padStart(4, '0')
+  const temperatureHex = (desiredEnvironment.temperature * 10).toString(16).padStart(4, '0')
+  const co2Hex = desiredEnvironment.co2.toString(16).padStart(4, '0')
+  const payload = `${humidityHex}${temperatureHex}${co2Hex}`
+
+  try {
+    sendDownlinkMessage(payload, Number(plantId))
+    return res.status(200).json({ payload, status: 'success' })
+  } catch (error) {
+    return res.status(500).json({ message: 'Server error', status: 'error' })
+  }
 })
 
 environmentRouter.get('/:type', async (req: UserRequest, res) => {
