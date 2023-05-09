62abeea4543106e63c71b5a6a3734aeecb5b430e
Rafael Sánchez Córdoba <rafa.tivuron@gmail.com>
2023-05-09 17:58:24 +0200

    Responsive navbar v1

--- apps/client/components/layouts/DefaultLayout.tsx
+++ apps/client/components/layouts/DefaultLayout.tsx
@@ -1,19 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Navbar } from "../navigation/navbar/Navbar";
 import { Breadcrumbs } from "../navigation/breadcrumbs/Breadcrumbs";
 import styles from "./layouts.module.css";
+import { NavbarMobile } from "../navigation/navbar/NavbarMobile";
 
 interface Props{
   children: React.ReactNode;
 }
+
+
 export const DefaultLayout:React.FC<Props> = ({children}) => {
-  return (
-    <div className={styles.defaultWrapper}>
-      <Navbar />
-      <div className={styles.defaultInner}>
-        <Breadcrumbs />
-        {children}
+
+  const [isDesktop, setDesktop] = useState(false)
+
+  useEffect(() => {
+    window.addEventListener("resize", updateMedia)
+    return () => window.removeEventListener("resize", updateMedia)
+  })
+  
+  const updateMedia = () => {
+    if (window.innerWidth > 800) {
+      setDesktop(true)
+    } else {
+      setDesktop(false)
+    }
+  }
+
+
+  if (isDesktop) {
+    return (
+      <div className={styles.defaultWrapper}> 
+          <Navbar /> 
+        <div className={styles.defaultInner}>
+          <Breadcrumbs />
+          {children}
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className={styles.mobileWrapper}> 
+        <NavbarMobile /> 
+        <div className={styles.mobileInner}>
+          <Breadcrumbs />
+          {children}
+        </div>
+      </div>
+    );
+  }
 };
---
+++ apps/client/components/navigation/navbar/NavbarMobile.tsx
@@ -0,0 +1,61 @@
+import React, { useState, useRef } from 'react';
+import styles from './NavbarMobile.module.css'
+import { NavItem } from '../navItem/NavItem'
+import { MdDashboard, MdFormatListBulletedAdd, MdDeviceThermostat } from 'react-icons/md'
+import { RiPlantFill } from 'react-icons/ri'
+import { UserButton } from '../userButton/UserButton'
+import { LoginModal } from '../../login/LoginModal'
+import { FaBars, FaTimes } from "react-icons/fa";
+
+export const NavbarMobile: React.FC = () => {
+    const [displayLoginModal, setDisplayLoginModal] = useState(false)
+    const [showFullNav, setShowFullNav] = useState(false)
+
+
+    if (!showFullNav) {
+        return (
+            <div className={styles.navbarWrapper}>
+                <div className={styles.headerNav}>
+                    <h1 className={styles.title}>Plantify</h1>
+                    <button className={styles.navbar_close_btn}
+                        onClick= {() => {setShowFullNav(true)}}>
+                        <FaBars />
+                    </button> 
+                </div> 
+            </div>
+        )
+    }
+    return (
+        <div className={styles.fullNavbarWrapper}>
+            <div className={styles.headerNav}>
+                <h1 className={styles.title}>Plantify</h1>
+                <button className={styles.nav_btn}
+                    onClick= {() => {setShowFullNav(false)}}>
+                    <FaTimes />
+                </button>
+            </div>
+            <div className={styles.navbarInner}>
+                <NavItem path={'/'}>
+                    <MdDashboard size={20} color={'#5BA937'} />
+                    <p>Dashboard</p>
+                </NavItem>
+                <NavItem path={'/Plants'}>
+                    <RiPlantFill size={20} color={'#5BA937'} />
+                    <p>Plants</p>
+                </NavItem>
+                <NavItem path={'/'}>
+                    <MdFormatListBulletedAdd size={20} color={'#5BA937'} />
+                    <p>Tasks</p>
+                </NavItem>
+                <NavItem path={'/'}>
+                    <MdDeviceThermostat size={20} color={'#5BA937'} />
+                    <p>Environment</p>
+                </NavItem>
+                <UserButton onClick={() => setDisplayLoginModal(!displayLoginModal)} />
+                {/*conditional rendering dependent on state*/}
+                {displayLoginModal && <LoginModal onClose={() => setDisplayLoginModal(false)} />}
+                
+            </div>
+        </div>
+    )
+}
