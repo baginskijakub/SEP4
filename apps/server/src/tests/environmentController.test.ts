b66bbead59b0f5e5b8389e2c3941471521e242f4
Franciszek Nurkiewicz <318212@via.dk>
2023-05-12 00:06:28 +0200

    endpoint tests (patch) for error codes 400 and 200

--- apps/server/src/tests/environmentController.test.ts
+++ apps/server/src/tests/environmentController.test.ts
@@ -19,7 +19,7 @@ describe('Patch environment', () => {
 
     //create plant so we can patch its environment
     const plant = await prisma.plant.create({
-      data:{
+      data: {
         id: 1,
         name: 'Test plant',
         nickName: 'A plant for testing',
@@ -37,7 +37,6 @@ describe('Patch environment', () => {
 
     plantId = plant.id
 
-    
     const loginResponse = await request(app).get('/api/v1/users').query({
       username: 'test_user',
       password: 'Password123',
@@ -51,6 +50,7 @@ describe('Patch environment', () => {
     await prisma.user.deleteMany()
   })
 
+  //test from plantPatchTest
   test('returns 400 status and error message when failed to update plant (not valid plantId)', async () => {
     const updatedPlant = {
       name: 'Updated plant name',
@@ -74,14 +74,49 @@ describe('Patch environment', () => {
     expect(response.body.status).toBe('error')
   })
 
+  //get, error 400, wrong type
   test('returns 400 status and error message if type is invalid', async () => {
-    const response = await request(app).get(`/api/v1/plants/${plantId}/environment/invalid_type`).set('Cookie', authToken)
+    const response = await request(app)
+      .get(`/api/v1/plants/${plantId}/environment/invalid_type`)
+      .set('Cookie', authToken)
     expect(response.status).toBe(400)
     expect(response.body.message).toBe('Wrong type')
     expect(response.body.status).toBe('error')
   })
-  
 
+  //patch, error 400, missing ID
+  test('returns 400 error if plantId is missing', async () => {
+    const response = await request(app)
+      .patch('/api/v1/plants/invalidId/environment')
+      .set('Cookie', authToken)
+      .send({ humidity: 50, temperature: 25, co2: 800 })
 
+    expect(response.status).toBe(400)
+    expect(response.body.message).toBe('Missing plant id')
+    expect(response.body.status).toBe('error')
+  })
 
+  //patch, error 400, invalid desired environment
+  test('returns a 400 error if desired environment is invalid', async () => {
+    const response = await request(app)
+      .patch(`/api/v1/plants/${plantId}/environment`)
+      .set('Cookie', authToken)
+      .send({ humidity: 'invalid', temperature: 25, co2: 800 })
+
+    expect(response.status).toBe(400)
+    expect(response.body.message).toBe('Invalid desired environment')
+    expect(response.body.status).toBe('error')
+  })
+
+  //patch, code 200, success
+  test('sends a downlink message and returns a success response - 200', async () => {
+    const response = await request(app)
+      .patch(`/api/v1/plants/${plantId}/environment`)
+      .set('Cookie', authToken)
+      .send({ humidity: 50, temperature: 25, co2: 800 })
+
+    expect(response.status).toBe(200)
+    expect(response.body.status).toBe('success')
+    expect(response.body.payload).toBeDefined()
+  })
 })
