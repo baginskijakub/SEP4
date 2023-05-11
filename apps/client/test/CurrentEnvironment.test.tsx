c28c83f162ba47c4842745625fb8ceab5216bde5
Rosabm <rosabrialesm@gmail.com>
2023-05-11 14:28:43 +0200

    changed tests in current environment

--- apps/client/test/CurrentEnvironment.test.tsx
+++ apps/client/test/CurrentEnvironment.test.tsx
@@ -4,7 +4,7 @@ import '@testing-library/jest-dom'
 
 describe("<Environment />", () => {
     test("renders environment component", () => {
-        render(<PlantCurrentEnvironment environment={{temperature: 0, humidity: 0, co2: 0}}/>)
+        render(<PlantCurrentEnvironment id={1}/>)
 
         expect(screen.getByText("Current Environment")).toBeInTheDocument()
         expect(screen.getByText(/Temperature/i)).toBeInTheDocument()
@@ -12,16 +12,5 @@ describe("<Environment />", () => {
         expect(screen.getAllByText(/Environment/i)[0]).toBeInTheDocument()
     })
 
-    test("renders environment component with correct values", () => {
-        render(<PlantCurrentEnvironment environment={{temperature: 22, humidity: 54, co2: 80}}/>)
-
-        expect(screen.getByText("Current Environment")).toBeInTheDocument()
-        expect(screen.getByText(/Temperature/i)).toBeInTheDocument()
-        expect(screen.getByText("22°C")).toBeInTheDocument()
-        expect(screen.getByText(/Humidity/i)).toBeInTheDocument()
-        expect(screen.getByText("54")).toBeInTheDocument()
-        expect(screen.getAllByText(/Environment/i)[0]).toBeInTheDocument()
-        expect(screen.getByText("80")).toBeInTheDocument()
-    })
-
+   
 })
