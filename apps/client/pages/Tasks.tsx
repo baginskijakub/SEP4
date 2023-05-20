708e9fdbf510ed2d3f3f2b029718f3680bd35cb4
Rafael Sánchez Córdoba <rafa.tivuron@gmail.com>
2023-05-20 18:14:04 +0200

    Task page missing task list

--- apps/client/pages/Tasks.tsx
+++ apps/client/pages/Tasks.tsx
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import styles from "./styles/Plants.module.css";
 import { TaskOverview } from "../components/task/overview/TaskOverview";
+import { ITask } from "@sep4/types";
+import { useUserContext } from "../context/UserContext";
+import { getAllTasksWithEpoch } from "../services/TaskService";
 
-const Tasks:React.FC = () => {
+export const Tasks:React.FC = () => {
+  const [tasks, setTasks] = React.useState<ITask[]>([])
+  const {user} = useUserContext()
+
+  // useEffect(() => {
+  //   getAllTasksWithEpoch().then((res) => {
+  //     setTasks(res)
+  //   }).catch((e) => {
+  //     console.log(e)
+  //   })
+  // }, [])
+
+  // const fetchData = () => {
+  //   getAllTasksWithEpoch().then((res) => {
+  //     setTasks(res)
+  //   }).catch((e) => {
+  //     console.log(e)
+  //   })
+  // }
+
   return (
-    <div>
-      <TaskOverview />
+    <div className={styles.taskWrapper}>
+      <h2>TaskList...</h2>
+      {user && <TaskOverview />}
     </div>
   );
 };
 
 export default Tasks;
+
