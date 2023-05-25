96012d4d22d38cd4e5f4f042c1f6d0a215722efc
Rosabm <rosabrialesm@gmail.com>
2023-05-25 18:49:49 +0200

    fix services

--- apps/client/services/RegisterService.ts
+++ apps/client/services/RegisterService.ts
@@ -2,19 +2,21 @@ import axios from 'axios'
 axios.defaults.withCredentials = true
 
 import { SERVER_URL } from '../config'
+import { error, log } from 'console'
 const register = (email: string, password: string) => {
   return axios
     .post(`${SERVER_URL}/users`, {
       email,
       password,
-    })
-    .then((response) => {
+    }).catch((error) => {
+      throw new Error(error.message)
+    }).then((response) => {
       if (response.status === 201) {
         return response.data
       } else {
-        throw new Error('Error')
+        throw new Error(response.data.message)
       }
-    })
+    })    
 }
 
 export { register }
