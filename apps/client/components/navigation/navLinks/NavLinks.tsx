946216a9cb165e1d66fc4f26a5b2c0ec7335bb73
baginskijakub <92220319+baginskijakub@users.noreply.github.com>
2023-05-27 21:16:23 +0200

    Fix: NavLinks.tsx

--- apps/client/components/navigation/navLinks/NavLinks.tsx
+++ apps/client/components/navigation/navLinks/NavLinks.tsx
@@ -1,23 +1,20 @@
-import React, { useState } from 'react'
+import React from 'react'
 import styles from './NavLinks.module.css'
 import { NavItem } from '../navItem/NavItem'
-import { MdDashboard, MdFormatListBulletedAdd, MdDeviceThermostat } from 'react-icons/md'
+import { MdDashboard, MdFormatListBulletedAdd } from 'react-icons/md'
 import { RiPlantFill } from 'react-icons/ri'
-import { UserButton } from '../userButton/UserButton'
-import { LoginModal } from '../../login/LoginModal'
 
 interface Props {
   closeNav?: () => void
 }
 
 export const NavLinks: React.FC<Props> = ({ closeNav }) => {
-  const [displayLoginModal, setDisplayLoginModal] = useState(false)
 
   return (
     <div className={styles.navbarInner}>
       <NavItem path={'/'} onClick={closeNav}>
         <MdDashboard size={20} color={'#5BA937'} />
-        <p className={styles.navItem}>Dashboard</p>
+        <p className={styles.navItem}>Home</p>
       </NavItem>
       <NavItem path={'/Plants'} onClick={closeNav}>
         <RiPlantFill size={20} color={'#5BA937'} />
@@ -26,10 +23,6 @@ export const NavLinks: React.FC<Props> = ({ closeNav }) => {
       <NavItem path={'/Tasks'} onClick={closeNav}>
         <MdFormatListBulletedAdd size={20} color={'#5BA937'} />
         <p className={styles.navItem}>Tasks</p>
-      </NavItem>
-      <NavItem path={'/'} onClick={closeNav}>
-        <MdDeviceThermostat size={20} color={'#5BA937'} />
-        <p className={styles.navItem}>Environment</p>
       </NavItem>
     </div>
   )
