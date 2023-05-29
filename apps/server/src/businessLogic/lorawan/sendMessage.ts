2b7b6bd37294442d466cd027f88589bdc26deea5
Tomek Słoma <82450784+Hedrekao@users.noreply.github.com>
2023-05-29 18:00:37 +0200

    fix to sending data to lorawan

--- apps/server/src/businessLogic/lorawan/sendMessage.ts
+++ apps/server/src/businessLogic/lorawan/sendMessage.ts
@@ -1,15 +1,14 @@
 import { lorawanSocket } from '../../main'
 export function sendDownlinkMessage(data: string, port: number) {
-  //TODO handle confirmation message also in messageEvent
-  lorawanSocket.send(
-    JSON.stringify({
-      cmd: 'tx',
-      EUI: process.env.EUI,
-      port: 1,
-      confirmed: false,
-      data,
-    }),
-  )
+  const eui = process.env.EUI.toString().replace(/\n/, '')
+  const json = JSON.stringify({
+    cmd: 'tx',
+    EUI: eui,
+    port: 1,
+    confirmed: false,
+    data,
+  })
+  lorawanSocket.send(json)
 }
 
 export function sendQueryMessage(from?: number, to?: number, perPage?: number) {
